46097a2ce847c719fb0a0dde6a7168b01bfbd20f
CYYYY17 <68625532+CYYYY17@users.noreply.github.com>
2020-07-28 17:18:50 +0800

    without dgm(1,n)

---
+++ Modules/Demos/demo_NGM11.py
@@ -0,0 +1,10 @@
+from CommonOperation.Accumulation import Accumulation
+from CommonOperation.GreyModels import NGM
+import numpy as np
+x = [15, 16.1, 17.3, 18.4, 18.7, 19.6, 19.9, 21.3, 22.5]
+t = np.arange(len(x))+1
+ngm = NGM()
+ngm.fit(t, x)
+y_predict = ngm.predict(t)
+print(x)
+print(y_predict)
